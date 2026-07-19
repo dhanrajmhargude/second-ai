4a30cafa1f069a6f8de6a2ba235d488703e66183
Dhanraj <dhanrajmhargude03@gmail.com>
2026-07-19 14:25:56 +0530

    feat: implement signup API

--- apps/web/app/api/auth/signup/route.ts
+++ apps/web/app/api/auth/signup/route.ts
@@ -1,49 +1,80 @@
 import { NextRequest, NextResponse } from "next/server";
-import { safeParse } from "zod";
-import { signupSchema } from "@/lib/validation/auth";
-import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 
+import { signupSchema } from "@/lib/validation/auth";
+import { prisma } from "@/lib/prisma";
+
 export async function POST(req: NextRequest) {
-    const body = await req.json
-    const result = signupSchema.safeParse(body) 
+  try {
+    const body = await req.json();
+    const result = signupSchema.safeParse(body);
 
     if (!result.success) {
-        return NextResponse.json(
-            {
-                success: false,
-                errors: result.error.issues,
-            },
-            {
-                status: 400,
-            }
-        );
+      return NextResponse.json(
+        {
+          success: false,
+          errors: result.error.issues,
+        },
+        {
+          status: 400,
+        }
+      );
     }
 
-    const { fullName ,email, password } = result.data;
+    const { fullName, email, password } = result.data;
+
     const existingUser = await prisma.user.findUnique({
-        where : {
-            email
-            },
-        });
+      where: {
+        email,
+      },
+    });
 
     if (existingUser) {
-        return NextResponse.json(
-            {
-                success: false,
-            },
-            {
-                status: 409,
-            }
-        );
-
-        const hashedPassword = await bcrypt.hash(password, 12);
-        await prisma.user.create({
-            data: {
-                name: fullName,
-                email,
-                password: hashedPassword,
-            },
-        });
+      return NextResponse.json(
+        {
+          success: false,
+          error: "User already exists",
+        },
+        {
+          status: 409,
+        }
+      );
     }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prisma.user.create({
+      data: {
+        name: fullName,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        },
+      },
+      {
+        status: 201,
+      }
+    );
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      {
+        success: false,
+        error: "Internal Server Error",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
